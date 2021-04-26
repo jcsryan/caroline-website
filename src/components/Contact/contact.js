0f92113b6fb9125db16c4e65bcf9efc37844021e
JCSR <johnconnorsryan@gmail.com>
2021-04-26 16:28:31 -0400

    bulk entry done

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,7 +2,48 @@ import React from "react";
 
 function Contact() {
     return(
-      <p>test contact</p>
+      <div className="contact-wrapper">
+          <div className="contact-text">
+              <h1>Contact Me</h1>
+              <p>
+              I offer a free 15 minute telephone consultation to further explain my counseling philosophy 
+              and learn more about your goals for treatment. Please feel free to reach out with any questions. 
+              </p>
+              <br></br>
+              <br></br>
+              <p>
+              Caroline Ryan Counseling<br></br>
+                P.O. Box 637<br></br>
+            Essex, Massachusetts 01929<br></br>
+            caroline@cryancounseling.com<br></br>
+                978-238-0998<br></br>
+              </p>
+                <br></br>
+                <br></br>
+              <h3>***If you are in a crisis or any other person may be in danger - don't use this site. 
+                  These resources can provide you with immediate help***</h3>
+                <br></br>
+                <br></br>
+              <ul>
+                  <li>Emergency: 911</li>
+                  <li>National Domestic Violence Hotline: 1- 800-799-7233</li>
+                  <li>National Suicide Prevention Lifeline: 1-800-273-TALK (8255)</li>
+                  <li>National Hopeline Network: 1-800-SUICIDE (800-784-2433)</li>
+                  <li>Crisis Text Line: Text "DESERVE" TO 741-741</li>
+                  <li>Lifeline Crisis Chat (Online live messaging): https://suicidepreventionlifeline.org/chat/</li>
+                  <li>Self-Harm Hotline: 1-800-DONT CUT (1-800-366-8288)</li>
+                  <li>Essential local and community services: 211, https://www.211.org/</li>
+                  <li>Planned Parenthood Hotline: 1-800-230-PLAN (7526) </li>
+                  <li>National Council on Alcoholism & Drug Dependency Hope Line: 1-800-622-2255</li>
+                  <li>National Crisis Line - Anorexia and Bulimia: 1-800-233-4357</li>
+                  <li>GLBT Hotline: 1-888-843-4564</li>
+                  <li>TREVOR Crisis Hotline: 1-866-488-7386</li>
+                  <li>AIDS Crisis Line: 1-800-221-7044</li>
+                  <li>Veterans Crisis Line: https://www.veteranscrisisline.net</li>
+                  <li>TransLifeline: https://www.translifeline.org - 877-565-8860</li>
+              </ul>
+          </div>
+      </div>
     )
 }
 
--- src/components/Why/why.js
+++ src/components/Why/why.js
@@ -2,7 +2,35 @@ import React from "react";
 
 function Why() {
     return(
-      <p>test why</p>
+      <div className="why-wrapper">
+          <div className="why-text">
+              <h1>Why Telehealth?</h1>
+              <p>
+              Why do I provide therapy via telehealth?  While many health issues benefit from in-person care, 
+              due to the nature of talk-therapy, I find that the convenience of telehealth outweighs the 
+              benefits of meeting in person. We are able to commit to a regular therapy schedule without
+               factoring in added barriers such as finding transportation, commuting to and from an office,
+                finding childcare, or spreading illness.  As a result, far fewer appointments are missed and 
+                more time is spent focusing on our work together.
+                <br></br>
+                <br></br>
+              To streamline the process and fully safeguard our sessions, I use Doxy.me. Doxy.me is a HIPPA
+               compliant video conferencing platform for health professionals that offers a simple and secure
+                means for us to meet. It is web-based, so no download is required, and you don’t even have to
+                 create an account. All data is encrypted, your sessions are anonymous, and your personal 
+                 health information remains secure. 
+                 <br></br>
+                 <br></br> 
+              Accepting Blue Cross/Blue Shield of Massachusetts and private pay. Please confirm with your 
+              health insurance that you are covered for teletherapy sessions. 
+              <br></br>
+              <br></br>
+              To best take advantage of this system, appointment times should be scheduled for when you have 
+              access to a private space and are able to focus on our session. I look forward to seeing you!
+
+              </p>
+          </div>
+      </div>
     )
 }
 
